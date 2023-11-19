d71572db959f3c7633b4befa4bdba023b330d316
jomidiaz08 <johany.diaz0886@gmail.com>
2023-11-19 11:20:08 -0500

    parte 2

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-presentacion = input("ingresa tu nombre: ")
-print("Bienvenido, este es mi juego {}".format(presentacion))
+import readchar
+from readchar import key
+def main():
+    print("Presiona la tecla ↑. Para salir del bucle")
+
+    while True:
+        char = readchar.readkey()
+        print(f"Tecla presionada: {char}")
+        if char == key.UP:
+            break
+
+    print("Bucle finalizado.")
+
+if __name__ == "__main__":
+    main()
