b81e25bc2b480ab0ee800e2d4f9bcf337dd6d306
jomidiaz08 <johany.diaz0886@gmail.com>
2023-11-26 09:11:53 -0500

    parte 4

--- main.py
+++ main.py
@@ -1,22 +1,75 @@
-import readchar 
+import readchar
 import os
+from readchar import key
+
 def clear_terminal():
+    os.system("cls" if os.name == "nt" else "clear")
+    print("\n" * 100)
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+def cadena_a_matriz(cadena_maze):
+    filas_maze = cadena_maze.strip().split("\n")
+    matriz_maze = [list(fila) for fila in filas_maze]
+    return matriz_maze
 
-def main():
-    numero = 0
+def encontrar_posiciones_inicio_y_fin(matriz_maze):
+    for i in range(len(matriz_maze)):
+        for j in range(len(matriz_maze[i])):
+            if matriz_maze[i][j] == "P":
+                posicion_inicio = (i, j)
+            elif matriz_maze[i][j] == ".":
+                posicion_fin = (i, j)
+    return posicion_inicio, posicion_fin
 
-    while numero <= 50:
-    
-        clear_terminal()
+def principal(mapa):
+    matriz_maze = cadena_a_matriz(mapa)
+    posicion_inicio, posicion_fin = encontrar_posiciones_inicio_y_fin(matriz_maze)
+    px, py = posicion_inicio
 
-        print(f"Número: {numero}")
+    while (px, py) != posicion_fin:
+        limpiar()
+        for fila in matriz_maze:
+            print("".join(fila))
 
-        key = readchar.readkey()
+        tecla = readchar.readkey()
+        if tecla == key.UP:
+            nueva_px, nueva_py = px - 1, py
+        elif tecla == key.DOWN:
+            nueva_px, nueva_py = px + 1, py
+        elif tecla == key.LEFT:
+            nueva_px, nueva_py = px, py - 1
+        elif tecla == key.RIGHT:
+            nueva_px, nueva_py = px, py + 1
+        else:
+            continue
+        if (
+            0 <= nueva_px < len(matriz_maze)
+            and 0 <= nueva_py < len(matriz_maze[0])
+            and matriz_maze[nueva_px][nueva_py] != "#"
+        ):
+            matriz_maze[px][py] = "."
+            px, py = nueva_px, nueva_py
+            matriz_maze[px][py] = "P"
 
-        if key == 'n':
-            numero += 1
+    clear_terminal()  
+
+def limpiar():
+    os.system("cls" if os.name == "nt" else "clear")
 
 if __name__ == "__main__":
-    main()
+    limpiar()
+    presentacion = input("ingresa tu nombre: ")
+    print("Bienvenido, este es mi juego {}".format(presentacion))
+
+    limpiar()
+    mapa = """
+##########
+#P.......#
+#.##..####
+#...#.#..#
+###.#.###.
+#.....#..#
+#.###.#..#
+#...#....#
+########.#
+"""
+    principal(mapa)
